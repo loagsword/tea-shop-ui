d571a06b05a2ebd7bd64954c567b76d47868e351
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2020-06-26 13:29:05 -0400

    added new dependencies and updated webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
+
 module.exports = {
   "output": {
-    "filename": "[name].pack.js"
+    "filename": "[name].pack.js",
+    "path": path.resolve(__dirname, 'docs')
   },
   "entry": {
-    "index": "./index"
+    "index": "./src/index"
   },
   "module": {
     "rules": [
